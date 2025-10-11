45c4ed70bb0a66bfe00f4c829f202348a271ee4d
Wai-Thura-Tun <maxw52493@gmail.com>
2025-10-11 19:34:51 +0700

    Initial Commit

---
+++ BookFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookFinder
+//
+//  Created by Wai Thura Tun on 11/10/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BookFinderTests/BookFinderTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookFinderTests.swift
+//  BookFinderTests
+//
+//  Created by Wai Thura Tun on 11/10/2568 BE.
+//
+
+import Testing
+@testable import BookFinder
+
+struct BookFinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
